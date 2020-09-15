e6c998efc6346c203bd4b07fccc5f91f7b099d80
Leonardo <leosud099@gmail.com>
2020-09-15 12:50:20 -0600

    first implementation of routes

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,44 +1,6 @@
 const http = require("http");
-const fs = require("fs"); // File System
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Enter message</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form></body>'
-    );
-    res.write("</html>");
-
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunck) => {
-      console.log(chunck);
-      body.push(chunck);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>Post Message</title></head>");
-  res.write("<body><h1>Message Received</h1></body>");
-  res.write("</html>");
-
-  return res.end();
-});
+const server = http.createServer(routes.requestHandler);
 
 server.listen(3000);
