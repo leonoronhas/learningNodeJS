5cf0f20ee5fe817ee81f894977d0ec5f13aec911
Leonardo <leosud099@gmail.com>
2020-10-02 16:52:28 -0600

    Finished section

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-// 404 Controller
-const errorController = require("./controllers/error");
-
-// Routers
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-// 404 page middleware
 app.use(errorController.get404);
 
-app.listen(3000, () => console.log(`Listening on port 3000`));
+app.listen(3000);
